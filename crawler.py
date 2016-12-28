40c2d06a6ebff234f10109ec6e6734be36108db8
themotionmachine <ryan.mail.email@gmail.com>
2016-12-28 16:01:03 -0600

    Added a Shelf for storing user credentials

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,5 +8,37 @@ from selenium.webdriver.common.keys import Keys
 import shelve
 import xlsxwriter
 import datetime
+driver = webdriver.Chrome('/Users/old/Desktop/2017M3S/chromedriver')
 
-test test
+def getUserInfo():
+    shelfFile = shelve.open('m3sshelf')
+
+    Username = raw_input('Please enter your Username:   ')
+    Password = raw_input('Please enter your Password:    ')
+    print('M3S_Scrape will now check to see if these credentials work.')
+    driver.get("https://m3s.tamu.edu")
+    time.sleep(5)
+    driver.find_element_by_id('UserName').send_keys(Username)
+    driver.find_element_by_id('Password').send_keys(Password)
+    driver.find_element_by_class_name('DisplayButton').click()
+    time.sleep(5)
+    if len(driver.find_elements_by_id('Password')) == 0:
+        shelfFile['un'] = Username
+        shelfFile['pw'] = Password
+        shelfFile.close()
+        return
+    else:
+        getUserInfo()
+
+
+
+def checkForShelve():
+    cd = os.getcwd()
+    if os.path.exists(os.path.join(cd, 'm3sshelf.db')):
+        return True
+    else:
+        getUserInfo()
+
+checkForShelve()
+
+
